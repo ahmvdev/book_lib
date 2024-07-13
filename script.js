5ce742c7cfc9f9c4e0903bd2f10b8fc1b1c27a7f
candy <ahmadsaeed3290@gmail.com>
2024-07-14 03:30:22 +0500

    added files and form displays

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+let bookAdd = document.getElementById("btn-add")
+let form = document.querySelector(".style-book")
+
+
+bookAdd.addEventListener("click", function(){
+    form.style.visibility = "visible";
+})
