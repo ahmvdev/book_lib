3eeb90c6b79b3459ab37b628bf3f24cef0ff9942
candy <ahmadsaeed3290@gmail.com>
2024-07-15 12:12:49 +0500

    restarting again

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+const myLibrary = [];
 
+function Book() {
+  // the constructor...
+}
 
-
-let bookAdd = document.getElementById("btn-add")
-let form = document.querySelector(".style-book")
-
-
-bookAdd.addEventListener("click", function(){
-    form.style.visibility = "visible";
-})
+function addBookToLibrary() {
+  // do stuff here
+}
